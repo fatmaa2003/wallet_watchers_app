3e15ece9f5a620c2b755fae560c89169aea9b4e5
salHarb <salma.hossam.harb@gmail.com>
2025-03-19 20:23:21 +0200

    Init api logic

---
+++ backend/model/categories.model.js
@@ -0,0 +1,24 @@
+const mongoose = require ('mongoose');
+const db = require('../config/db');
+
+
+const { Schema } = mongoose;
+
+const categoriesSchema = new Schema({
+
+    categoryId:{
+        type: Number,
+        default: 0,
+    },
+    categoryName:{
+        type: String,
+        default: 0
+    }
+
+    //add category id and category name instead of user id and remove ell category names and add them as enteries from db.
+   
+}, { timestamps: true });
+
+const categoriesModel = mongoose.model('categories',categoriesSchema);
+
+module.exports = categoriesModel;
