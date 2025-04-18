c55469fab5cf3d34d7437a9d83ee1d76075ca1f0
salHarb <salma.hossam.harb@gmail.com>
2025-04-18 20:37:10 +0200

    users

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require("../controller/users.controller");
 
 router.post("/signup", usersController.signup);
-router.post("/login", usersController.login);
+router.post("/postLogin", usersController.login);
 router.post("/addexpense", usersController.addExpense);
 
 module.exports = router;
